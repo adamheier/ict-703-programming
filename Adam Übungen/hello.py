1ab0f246b4be02e66f091f20fc2ee5b329747687
adamheier <adamheier7@gmail.com>
2026-07-22 11:37:55 +1000

    vault backup: 2026-07-22 11:37:55

--- Adam Übungen/hello.py
+++ Adam Übungen/hello.py
@@ -1,1 +1,12 @@
-print ("Hello World!")
+print("Hello World!")
+
+name = input("What is your name? ")
+print("Hello " + name + ", how are you today?")
+
+friend_name = input("What is your friends name? ")
+
+'''Happy Birthday!'''
+name = input("What is your name: ")
+print ("Happy Birthday " + name + "!")
+
+
